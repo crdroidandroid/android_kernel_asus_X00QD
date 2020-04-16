c0eac546a99288a677f0564e664f21bbbe00ccb4
emincangencer <emincange@gmail.com>
2020-04-16 17:33:30 +0000

    UPSTREAM: HID: sony: Define needed vars and fix duplicates

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -53,6 +53,8 @@
 #define MOTION_CONTROLLER_BT      BIT(9)
 #define NAVIGATION_CONTROLLER_USB BIT(10)
 #define NAVIGATION_CONTROLLER_BT  BIT(11)
+#define SINO_LITE_CONTROLLER      BIT(12)
+#define FUTUREMAX_DANCE_MAT       BIT(13)
 
 #define to_hid_device(pdev) container_of(pdev, struct hid_device, dev)
 
@@ -1468,6 +1470,8 @@ out:
 
 static int sixaxis_set_operational_bt(struct hid_device *hdev)
 {
+        static const u8 report[] = { 0xf4, 0x42, 0x03, 0x00, 0x00 };
+	u8 *buf;
 	int ret;
 
 	buf = kmemdup(report, sizeof(report), GFP_KERNEL);
@@ -1488,6 +1492,7 @@ static int sixaxis_set_operational_bt(struct hid_device *hdev)
  */
 static int dualshock4_get_calibration_data(struct sony_sc *sc)
 {
+        u8 *buf;
 	int ret;
 	short gyro_pitch_bias, gyro_pitch_plus, gyro_pitch_minus;
 	short gyro_yaw_bias, gyro_yaw_plus, gyro_yaw_minus;
@@ -2725,68 +2730,6 @@ err_stop:
 	kfree(sc->output_report_dmabuf);
 	sony_remove_dev_list(sc);
 	sony_release_device_id(sc);
-	return ret;
-}
-
-static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
-{
-	int ret;
-	unsigned long quirks = id->driver_data;
-	struct sony_sc *sc;
-	unsigned int connect_mask = HID_CONNECT_DEFAULT;
-
-	sc = devm_kzalloc(&hdev->dev, sizeof(*sc), GFP_KERNEL);
-	if (sc == NULL) {
-		hid_err(hdev, "can't alloc sony descriptor\n");
-		return -ENOMEM;
-	}
-
-	spin_lock_init(&sc->lock);
-
-	sc->quirks = quirks;
-	hid_set_drvdata(hdev, sc);
-	sc->hdev = hdev;
-
-	ret = hid_parse(hdev);
-	if (ret) {
-		hid_err(hdev, "parse failed\n");
-		return ret;
-	}
-
-	if (sc->quirks & VAIO_RDESC_CONSTANT)
-		connect_mask |= HID_CONNECT_HIDDEV_FORCE;
-	else if (sc->quirks & SIXAXIS_CONTROLLER)
-		connect_mask |= HID_CONNECT_HIDDEV_FORCE;
-
-	/* Patch the hw version on DS3/4 compatible devices, so applications can
-	 * distinguish between the default HID mappings and the mappings defined
-	 * by the Linux game controller spec. This is important for the SDL2
-	 * library, which has a game controller database, which uses device ids
-	 * in combination with version as a key.
-	 */
-	if (sc->quirks & (SIXAXIS_CONTROLLER | DUALSHOCK4_CONTROLLER))
-		hdev->version |= 0x8000;
-
-	ret = hid_hw_start(hdev, connect_mask);
-	if (ret) {
-		hid_err(hdev, "hw start failed\n");
-		return ret;
-	}
-
-	/* sony_input_configured can fail, but this doesn't result
-	 * in hid_hw_start failures (intended). Check whether
-	 * the HID layer claimed the device else fail.
-	 * We don't know the actual reason for the failure, most
-	 * likely it is due to EEXIST in case of double connection
-	 * of USB and Bluetooth, but could have been due to ENOMEM
-	 * or other reasons as well.
-	 */
-	if (!(hdev->claimed & HID_CLAIMED_INPUT)) {
-		hid_err(hdev, "failed to claim input\n");
-		hid_hw_stop(hdev);
-		return -ENODEV;
-	}
-
 	return ret;
 }
 
