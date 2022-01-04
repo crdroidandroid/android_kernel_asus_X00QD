b934e8d454397c3983c2b6ccfcc4a59946b2e305
Kazuki Hashimoto <kazukih@tuta.io>
2022-01-05 00:06:44 +0900

    drivers: lpm-levels: Mark functions as static
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1359,7 +1359,7 @@ static inline void cpu_unprepare(struct lpm_cluster *cluster, int cpu_index,
 		msm_jtag_restore_state();
 }
 
-int get_cluster_id(struct lpm_cluster *cluster, int *aff_lvl)
+static int get_cluster_id(struct lpm_cluster *cluster, int *aff_lvl)
 {
 	int state_id = 0;
 
@@ -1388,7 +1388,7 @@ unlock_and_return:
 }
 
 #if !defined(CONFIG_CPU_V7)
-bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
+static bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 {
 	/*
 	 * idx = 0 is the default LPM state
@@ -1423,7 +1423,7 @@ bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 	}
 }
 #elif defined(CONFIG_ARM_PSCI)
-bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
+static bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 {
 	if (!idx) {
 		stop_critical_timings();
@@ -1448,7 +1448,7 @@ bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 	}
 }
 #else
-bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
+static bool psci_enter_sleep(struct lpm_cluster *cluster, int idx, bool from_idle)
 {
 	WARN_ONCE(true, "PSCI cpu_suspend ops not supported\n");
 	return false;
